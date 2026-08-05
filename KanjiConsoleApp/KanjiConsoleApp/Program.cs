ac3366bfeb74a02f436204aaaf43b68995954e6b
nguyentanmanh-dev <nguyentanmanh.dev@gmail.com>
2026-08-05 21:03:42 +0700

    Clean up .vs, bin, obj folders using wildcard pathspec

--- KanjiConsoleApp/KanjiConsoleApp/Program.cs
+++ KanjiConsoleApp/KanjiConsoleApp/Program.cs
@@ -8,16 +8,13 @@ List<LearningItem > myStudyList = new List<LearningItem>();
 myStudyList.Add(new Kanji("水", "スイ", "みず", "Nước", 4, "N5"));
 myStudyList.Add(new Kanji("火", "カ", "ひ", "Lửa", 4, "N5"));
 
-// 3. Thêm Vocabulary (Lớp con 2)
 myStudyList.Add(new Vocabulary("がくせい (Gakusei)", "Sinh viên"));
 myStudyList.Add(new Vocabulary("せんせい (Sensei)", "Giáo viên"));
 
 Console.WriteLine("=== DANH SÁCH HỌC TẬP HÔM NAY ===\n");
 
-// 4. Vòng lặp Đa hình (Polymorphism)
 foreach (LearningItem item in myStudyList)
 {
-    // Chỉ gọi 1 hàm duy nhất, nhưng mỗi object sẽ tự biết cách in ra kiểu của nó
     item.Display();
 }
 
